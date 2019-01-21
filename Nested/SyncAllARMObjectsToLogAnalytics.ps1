9309769c5e8601c40ace49df61ad1982b7eb14bf
wkasdorp <wkasdorp@hotmail.com>
2019-01-21 22:50:10 +0100

    .

--- Nested/SyncAllARMObjectsToLogAnalytics.ps1
+++ Nested/SyncAllARMObjectsToLogAnalytics.ps1
@@ -1,13 +1,26 @@
 # Get information required for the automation account from parameter values when the runbook is started.
 Param
 (
+    # target Log Analytics workspace
     [Parameter(Mandatory = $True)]
     [string]$workspacename,
+    # name of the custom log where data is written.
     [Parameter(Mandatory = $True)]
     [string]$logName,
+    # List of subscriptions (GUIDs) to sync. Default: subscripion of the automation account. 
+    # runbook input example: [b40a7802-5e7e-47d1-8011-e2b433bfb04f,0ff9f697-f328-4943-b2f5-54b37ae27f1a]
     [Parameter(Mandatory = $false)]
-    [Guid[]]$subscriptionIDList
+    [Guid[]]$subscriptionIDList,
+    # List of tag names to sync. Each name will have its own column.
+    # runbook input example: ["owner","bo-number"]
+    [Parameter(Mandatory = $false)]
+    [String[]]$tagnameList
 )
+
+#
+# maximum number of objects to send in one shot. 30 MB is the max.
+#
+$batchSize = 1000
 
 #
 # import modules before we start verbose mode... The OMS one is not present in the account by default. 
@@ -41,6 +54,9 @@ $workspaceKeys = $workspace | Get-AzureRmOperationalInsightsWorkspaceSharedKeys 
 $customerId = $workspace.CustomerId
 $sharedKey = $workspaceKeys.PrimarySharedKey
 
+Write-Verbose "- list of tags to be added to the logfile:"
+$tagnameList -join ', ' | Write-Verbose
+
 #
 # loop over subscriptions
 #
@@ -59,17 +75,38 @@ foreach ($subscriptionID in $subscriptionIDlist)
 
     $resourceList = @()
     Write-Verbose "NOTE: for sure we will need to push data in chunks, the REST body will be limited?"
-    # TODO: add tag processing, maybe Subscription Name for convenience
-    $resourceList += Get-AzureRmResource | select-object -First 100 | ForEach-Object {
-        [pscustomobject]@{
+    $resourceList += Get-AzureRmResourceGroup -PipelineVariable rg | ForEach-Object {
+        $record = [pscustomobject]@{
             SubscriptionId = $subscriptionId.ToString()
             SubscriptionName = $context.SubscriptionName
-            ResourceGroupName = $_.ResourceGroupName
-            Name = $_.Name
-            Location = $_.Location
-            ResourceType = $_.ResourceType
+            ResourceGroupName = $rg.ResourceGroupName
+            Name = $rg.ResourceGroupName
+            Location = $rg.Location
+            ResourceType = "(ResourceGroup)"
+        }        
+        foreach ($tagname in $tagnameList)
+        {
+            $record | Add-Member -MemberType NoteProperty -Name "tag-$($tagname)" -Value $_.tags.$tagname
         }
-    } 
+        $record
+    
+        Get-AzureRmResource -ResourceGroupName $rg.ResourceGroupName -PipelineVariable resource | ForEach-Object {
+            $record = [pscustomobject]@{
+                SubscriptionId = $subscriptionId.ToString()
+                SubscriptionName = $context.SubscriptionName
+                ResourceGroupName = $rg.ResourceGroupName
+                Name = $resource.Name
+                Location = $resource.Location
+                ResourceType = $resource.ResourceType
+            }
+            
+            foreach ($tagname in $tagnameList)
+            {
+                $record | Add-Member -MemberType NoteProperty -Name "tag-$($tagname)" -Value $_.tags.$tagname
+            }
+            $record
+        }
+    }  
 
     if ($null -ne $resourceList) {
         # Convert the job data to json
